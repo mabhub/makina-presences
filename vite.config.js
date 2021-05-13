ec20b154fe72ed86dc6c63f20ed69d98905e4198
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2021-05-13 17:26:56 +0200

    Add & enable ViteJS plugin for ESLint

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import viteESLint from '@ehutch79/vite-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [
+    reactRefresh(),
+    viteESLint(),
+  ],
 });
