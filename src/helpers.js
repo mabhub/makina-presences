9586eb5348486c09da23a9c6625f18238a5a6186
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2021-05-26 09:59:21 +0200

    Avoid app crash when no tri

--- src/helpers.js
+++ src/helpers.js
@@ -2,6 +2,6 @@ export const asDayRef = date => (
   date.year() * 1000 + date.dayOfYear()
 );
 
-export const nrmlStr = str => str.toLocaleLowerCase().trim();
+export const nrmlStr = (str = '') => str.toLocaleLowerCase().trim();
 
 export const sameLowC = (a, b) => (nrmlStr(a) === nrmlStr(b));
