dfb1cae8d513ecf61c36888a412247abcdfda327
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2022-03-16 14:42:25 +0100

    Create proxy function to query counts

---
+++ functions/list.js
@@ -0,0 +1,35 @@
+const baserowHeaders = {
+  Authorization: `Token ${process.env.TT_BASEROW_TOKEN}`,
+  'Content-Type': 'application/json',
+};
+
+const fetch = async (...args) => {
+  const { default: f } = await import('node-fetch');
+  return f(...args);
+};
+
+const fetchJson = async (...args) => {
+  const raw = await fetch(...args);
+  return raw.json();
+};
+
+exports.handler = async () => {
+  const response = await fetchJson(
+    [
+      `${process.env.TT_BASEROW_TABLE}?`,
+      'user_field_names=true',
+      'include=tri,total,enabled',
+      'size=200',
+    ].join('&'),
+    { headers: baserowHeaders },
+  );
+
+  const results = response.results
+    .filter(({ enabled }) => enabled)
+    .map(({ tri, total }) => ({ tri, total }));
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(results, null, 2),
+  };
+};
