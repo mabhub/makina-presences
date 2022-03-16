958be0699f0f5085a42e69982c5a0059e98d1f71
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2022-03-16 16:40:46 +0100

    Create config for allow using remote lambda in dev mode

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import viteESLint from '@ehutch79/vite-eslint';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.local' });
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +11,12 @@ export default defineConfig({
     reactRefresh(),
     viteESLint(),
   ],
+  server: {
+    proxy: {
+      '^/.netlify': {
+        target: process.env.LAMBDA_ROOT,
+        changeOrigin: true,
+      },
+    },
+  },
 });
