4669e4613c561665520c1feadd2a1ad02d193150
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2023-10-17 13:57:23 +0200

    Change style of multiple days & excess days

--- src/components/TTOLine.jsx
+++ src/components/TTOLine.jsx
@@ -8,39 +8,44 @@ const SIZE = 0.75;
 const TTOLine = ({ dates = [] }) => {
   const total = dates.reduce((acc, { days: d = 0 }) => (acc + d), 0);
 
+  let inc = 0;
   return (
     <>
-      {dates.map(date => (
-        <React.Fragment key={date.from}>
-          {[...Array(date.days)].map((_, index) => (
-            <React.Fragment
-              key={date.from + index}
-            >
-              {Boolean(index) && (
+      {dates.map((date, i) => (
+        <React.Fragment key={date.from + i}>
+          {[...Array(date.days)].map((_, index) => {
+            inc += 1;
+
+            return (
+              <React.Fragment key={date.from + index}>
+                {Boolean(index) && (
+                  <Box
+                    sx={{
+                      display: 'inline-block',
+                      width: '1px',
+                      height: `${SIZE}em`,
+                      background: inc <= 30 ? '#5a5a' : '#daaa',
+                      verticalAlign: 'middle',
+                      borderTop: `1px solid ${inc <= 30 ? '#5a5' : '#daa'}`,
+                      borderBottom: `1px solid ${inc <= 30 ? '#5a5' : '#daa'}`,
+                    }}
+                  />
+                )}
                 <Box
+                  title={date.from}
                   sx={{
-                    display: 'inline-block',
-                    width: '1px',
+                    width: `${SIZE}em`,
                     height: `${SIZE}em`,
-                    background: 'orange',
+                    display: 'inline-block',
+                    border: `1px solid ${inc <= 30 ? '#5a5' : '#daa'}`,
+                    background: inc <= 30 ? '#5a5' : '#daa',
+                    ml: !index ? '1px' : 0,
                     verticalAlign: 'middle',
                   }}
                 />
-              )}
-              <Box
-                title={date.from}
-                sx={{
-                  width: `${SIZE}em`,
-                  height: `${SIZE}em`,
-                  display: 'inline-block',
-                  border: '1px solid #55aa55',
-                  background: '#55aa55',
-                  ml: !index ? '1px' : 0,
-                  verticalAlign: 'middle',
-                }}
-              />
-            </React.Fragment>
-          ))}
+              </React.Fragment>
+            );
+          })}
         </React.Fragment>
       ))}
 
