59cdbb495694a2751658ba77a9de7d7c9b37e148
Antoine Mazeau <antoine.mazeau@makina-corpus.com>
2024-08-14 09:41:37 +0200

    Allow to undo/redo each modifications

--- src/components/admin/ActionBar.jsx
+++ src/components/admin/ActionBar.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
-import makeStyles from '@mui/styles/makeStyles';
+import { KeyboardArrowLeft, KeyboardArrowRight, Place } from '@mui/icons-material';
 import { Box } from '@mui/material';
+import makeStyles from '@mui/styles/makeStyles';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import createPersistedState from 'use-persisted-state';
 
 const useStyles = makeStyles(theme => {
   const maxWidth = mq => `@media (max-width: ${theme.breakpoints.values[mq]}px)`;
@@ -8,16 +11,87 @@ const useStyles = makeStyles(theme => {
   return {
     root: {
       width: '100%',
-      background: 'red',
+      // background: 'red',
+      padding: theme.spacing(0.5),
+      borderBottom: '2px solid #00000015',
+      display: 'flex',
+    },
+    button: {
+      display: 'flex',
+      padding: theme.spacing(0.3),
+      border: 'unset',
+      background: 'unset',
+      borderRadius: '5px',
+      transition: 'all 100ms cubic-bezier(0.4, 0, 0.2, 1)',
+      '&:hover': {
+        transition: 'all 100ms cubic-bezier(0.4, 0, 0.2, 1)',
+        background: 'rgba(0, 0, 0, 0.05);',
+        cursor: 'pointer',
+      },
     },
   };
 });
 
+const useUpdateStack = createPersistedState('updateStack');
+const useUndidStack = createPersistedState('undidStack');
+
 function ActionBar () {
   const classes = useStyles();
+  const { place } = useParams();
+
+  const [updateStack, setUpdateStack] = useUpdateStack({});
+  const [undidStack, setUndidStack] = useUndidStack({});
+
+  const handleUndo = () => {
+    setUndidStack({
+      ...undidStack,
+      [place]: [
+        ...undidStack[place],
+        updateStack[place][updateStack[place].length - 1],
+      ],
+    });
+    setUpdateStack({
+      ...updateStack,
+      [place]: [
+        ...updateStack[place].slice(0, -1),
+      ],
+    });
+  };
+
+  const handleRedo = () => {
+    setUpdateStack({
+      ...updateStack,
+      [place]: [
+        ...updateStack[place],
+        undidStack[place][undidStack[place].length - 1],
+      ],
+    });
+    setUndidStack({
+      ...undidStack,
+      [place]: [
+        ...undidStack[place].slice(0, -1),
+      ],
+    });
+  };
+
   return (
     <Box className={classes.root}>
-      test
+      <Box
+        className={classes.button}
+        component="button"
+        disabled={updateStack[place].length === 0}
+        onClick={handleUndo}
+      >
+        <KeyboardArrowLeft />
+      </Box>
+      <Box
+        className={classes.button}
+        component="button"
+        disabled={undidStack[place].length === 0}
+        onClick={handleRedo}
+      >
+        <KeyboardArrowRight />
+      </Box>
     </Box>
   );
 }
