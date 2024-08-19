5e8ff55949087d3ea6fb9af4aa4704a5996c8e74
Antoine Mazeau <antoine.mazeau@makina-corpus.com>
2024-08-19 14:27:49 +0200

    Allow to create a new spot

---
+++ src/components/admin/SpotForm.jsx
@@ -0,0 +1,140 @@
+import { Chip, FormControl, List, ListItem, ListItemText, MenuItem, Select, Switch, TextField } from '@mui/material';
+import makeStyles from '@mui/styles/makeStyles';
+import clsx from 'clsx';
+import React from 'react';
+import useFields from '../../hooks/useFields';
+
+const { VITE_TABLE_ID_SPOTS: spotsTableId } = import.meta.env;
+
+const useStyles = makeStyles(theme => ({
+  textField: {
+    '& .MuiOutlinedInput-input': {
+      padding: theme.spacing(0.5, 1),
+    },
+    width: '75px',
+    marginLeft: theme.spacing(0.5),
+  },
+  idTextField: {
+    '& .MuiOutlinedInput-input': {
+      padding: theme.spacing(0.5, 1),
+    },
+    '& .MuiFormHelperText-root': {
+      margin: theme.spacing(0),
+      width: '130px',
+      transform: 'translateX(-24%)',
+      fontSize: '10px',
+    },
+    width: '90px',
+  },
+  idLabel: {
+    transform: 'translateY(-25%)',
+  },
+}));
+
+function SpotForm ({ edit, isIDValid, spotInfo, handleChange }) {
+  const classes = useStyles();
+  const fields = useFields(spotsTableId);
+  const spotTypes = fields
+    ?.find?.(({ name }) => name === 'Type')
+    ?.select_options || [];
+
+  return (
+    <List disablePadding sx={{}}>
+      {!edit && (
+        <ListItem disablePadding>
+          <ListItemText
+            className={clsx({ [classes.idLabel]: !isIDValid })}
+          >Identifiant <strong>*</strong>
+          </ListItemText>
+          <TextField
+            required
+            className={classes.idTextField}
+            onChange={event => handleChange('Identifiant', event.target.value)}
+            error={!isIDValid}
+            helperText={isIDValid ? '' : 'Cet identifiant existe déjà'}
+          />
+        </ListItem>
+      )}
+      <ListItem disableGutters>
+        <ListItemText>Position</ListItemText>
+        <TextField
+          label="X"
+          variant="outlined"
+          size="small"
+          className={classes.textField}
+          value={spotInfo?.x}
+          type="number"
+          onChange={event => handleChange('x', event.target.value)}
+        />
+        <TextField
+          label="Y"
+          variant="outlined"
+          size="small"
+          className={classes.textField}
+          value={spotInfo.y}
+          type="number"
+          onChange={event => handleChange('y', event.target.value)}
+        />
+
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemText>Bloqué</ListItemText>
+        <Switch
+          onChange={() => handleChange('Bloqué', !spotInfo.Bloqué)}
+          checked={spotInfo.Bloqué}
+        />
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemText>Cumulable</ListItemText>
+        <Switch
+          onChange={() => handleChange('Cumul', !spotInfo.Cumul)}
+          checked={spotInfo.Cumul}
+        />
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemText>Type {!edit && (<strong>*</strong>)}</ListItemText>
+        <FormControl size="small">
+          <Select
+            value={spotInfo.Type ? spotInfo.Type.value : ''}
+            onChange={event => handleChange('Type', spotTypes.find(({ value }) => value === event.target.value))}
+            required
+          >
+            {spotTypes.map(({ value, color }) => (
+              <MenuItem value={value} key={value}>
+                <Chip
+                  label={value}
+                  sx={{
+                    background: color.replace('-', ''),
+                    opacity: 0.5,
+                    color: 'transparent',
+                  }}
+                />
+                <Chip
+                  label={value}
+                  sx={{
+                    background: 'transparent',
+                    position: 'absolute',
+                    left: 14,
+                  }}
+                />
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </ListItem>
+      <ListItem disableGutters>
+        <TextField
+          fullWidth
+          label="Description"
+          variant="outlined"
+          value={spotInfo.Description || ''}
+          multiline
+          rows={4}
+          onChange={event => handleChange('Description', event.target.value)}
+        />
+      </ListItem>
+    </List>
+  );
+}
+
+export default React.memo(SpotForm);
--- src/components/admin/SpotPanel.jsx
+++ src/components/admin/SpotPanel.jsx
@@ -1,49 +1,35 @@
 import { Close } from '@mui/icons-material';
-import { alpha, Avatar, Button, Card, CardActionArea, CardActions, CardContent, CardHeader, Chip, FormControl, IconButton, List, ListItem, ListItemText, MenuItem, Select, Switch, TextField, Typography } from '@mui/material';
+import { alpha, Avatar, Button, Card, CardContent, CardHeader, IconButton, Typography } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import React, { useEffect, useState } from 'react';
-import createPersistedState from 'use-persisted-state';
-import useFields from '../../hooks/useFields';
+import SpotForm from './SpotForm';
 
-const { VITE_TABLE_ID_SPOTS: spotsTableId } = import.meta.env;
-
-const useStyles = makeStyles(theme => {
-  const maxWidth = mq => `@media (max-width: ${theme.breakpoints.values[mq]}px)`;
-  return {
-    cardEdit: {
-      padding: theme.spacing(1, 2, 0, 0),
+const useStyles = makeStyles(theme => ({
+  cardEdit: {
+    padding: theme.spacing(1, 2, 0, 0),
+  },
+  textField: {
+    '& .MuiOutlinedInput-input': {
+      padding: theme.spacing(0.5, 1),
     },
-    textField: {
-      '& .MuiOutlinedInput-input': {
-        padding: theme.spacing(0.5, 1),
-      },
-      width: '75px',
-      marginLeft: theme.spacing(0.5),
-    },
-    delete: {
-      width: '100%',
-      margin: theme.spacing(1, 0, 0, 0),
-      padding: theme.spacing(1.5, 0),
-      border: `1.5px solid ${alpha(theme.palette.error.light, 0.5)}`,
-      borderRadius: '8px',
-    },
-  };
-});
-
-const useUpdatedStack = createPersistedState('updateStack');
-const useUndidStack = createPersistedState('undidStack');
+    width: '75px',
+    marginLeft: theme.spacing(0.5),
+  },
+  delete: {
+    width: '100%',
+    margin: theme.spacing(1, 0, 0, 0),
+    padding: theme.spacing(1.5, 0),
+    border: `1.5px solid ${alpha(theme.palette.error.light, 0.5)}`,
+    borderRadius: '8px',
+  },
+}));
 
 export const DELETED_KEY = 'deleted';
 
 function SpotPanel ({ spot, onClose, handleUpdate }) {
   const classes = useStyles();
-  const fields = useFields(spotsTableId);
 
   const { Identifiant } = spot;
-
-  const spotTypes = fields
-    ?.find?.(({ name }) => name === 'Type')
-    ?.select_options || [];
 
   const [spotInfo, setSpotInfo] = useState(spot);
   const [previousSpotInfo, setPreviousSpotInfo] = useState(spotInfo);
@@ -103,85 +89,11 @@ function SpotPanel ({ spot, onClose, handleUpdate }) {
       />
       {/* <Divider sx={{ mx: '-16px' }} /> */}
       <CardContent>
-        <List disablePadding>
-          <ListItem disablePadding>
-            <ListItemText>Position</ListItemText>
-            <TextField
-              label="X"
-              variant="outlined"
-              size="small"
-              className={classes.textField}
-              value={spotInfo.x}
-              type="number"
-              onChange={event => handleChange('x', event.target.value)}
-            />
-            <TextField
-              label="Y"
-              variant="outlined"
-              size="small"
-              className={classes.textField}
-              value={spotInfo.y}
-              type="number"
-              onChange={event => handleChange('y', event.target.value)}
-            />
-
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemText>Bloqué</ListItemText>
-            <Switch
-              onChange={() => handleChange('Bloqué', !spotInfo.Bloqué)}
-              checked={spotInfo.Bloqué}
-            />
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemText>Cumulable</ListItemText>
-            <Switch
-              onChange={() => handleChange('Cumul', !spotInfo.Cumul)}
-              checked={spotInfo.Cumul}
-            />
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemText>Type</ListItemText>
-            <FormControl size="small">
-              <Select
-                value={spotInfo.Type.value}
-                onChange={event => handleChange('Type', spotTypes.find(({ value }) => value === event.target.value))}
-              >
-                {spotTypes.map(({ value, color }) => (
-                  <MenuItem value={value} key={value}>
-                    <Chip
-                      label={value}
-                      sx={{
-                        background: color.replace('-', ''),
-                        opacity: 0.5,
-                        color: 'transparent',
-                      }}
-                    />
-                    <Chip
-                      label={value}
-                      sx={{
-                        background: 'transparent',
-                        position: 'absolute',
-                        left: 14,
-                      }}
-                    />
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </ListItem>
-          <ListItem disableGutters>
-            <TextField
-              fullWidth
-              label="Description"
-              variant="outlined"
-              value={spotInfo.Description || ''}
-              multiline
-              rows={4}
-              onChange={event => handleChange('Description', event.target.value)}
-            />
-          </ListItem>
-        </List>
+        <SpotForm
+          edit
+          spotInfo={spotInfo}
+          handleChange={handleChange}
+        />
         <Button
           size="small"
           color="error"
